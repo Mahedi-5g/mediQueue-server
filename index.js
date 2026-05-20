431caecff4578d87b243e089ff7d2df2fb9b39ef
MAHEDI HASAN <mahedi5096@gmail.com>
2026-05-20 16:35:31 +0600

    get-json-data

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 
 const dotenv = require('dotenv');
 dotenv.config();
+
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT;
 
 const uri = process.env.MONGODB_URI;
 
+app.use(cors());
 app.use(express.json());
 
 const client = new MongoClient(uri, {
@@ -22,15 +25,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const db = client.db('mediQueue');
+    const tutorCollection = db.collection('tutors-data');
+
+    app.get('/tutors',async(req,res)=>{
+      const cursor = tutorCollection.find().limit(6);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+
+    app.get('/all-tutors',async(req,res)=>{
+      const cursor = tutorCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-   
-    await client.close();
+  } catch (error) {
+    console.log(error);
   }
 }
-run().catch(console.dir);
+run();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
